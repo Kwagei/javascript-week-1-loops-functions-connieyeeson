3ef0e3642b214ca08c84a117a1ba38d338c3bbe4
connieyeeson <connieyeeson@gmail.com>
2023-06-08 23:31:50 +0000

    assignment

--- Assignment.js
+++ Assignment.js
@@ -16,21 +16,51 @@ function evenNumber(){
   } 
 evenNumber();
 
-// 2. A list of numbers 1 - 10 is given, write a function
-//  that prints the reverse of that that list
+ // 2. A list of numbers 1 - 10 is given, write a function
+ //  that prints the reverse of that that list
 
-let arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
-function reverselist(list){
+ let arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+  function reverselist(list){
 		list = [];
 		for ( let i = arr.length; i > 0; i-- ){
 		list += i;
-}
+ }
 		console.log(list);
-}
+ }
 	reverselist( arr);
 
     // 3. let arr = [1,5,30,56,3,48,67,44,7,32] write a function
     //  that prints that highest number
     //  in the array and prints the average of the numbers.
 
+    let arr = [ 1, 5, 30, 56, 3, 48, 67, 44, 7, 32 ];
+
+    function highestNum(num){
+		num = [];
+	for( let i = 0; i < arr.length; i++){
+
+	if( num < arr[i]){
+    num = arr[i];
+	
+  }
+  }
+
+		console.log(num);
+ } 
+	highestNum(arr);
+
+    // 4. A word is given write a punction to tell if the word is a
+    //  Palindrome
+
+ function givenwords(Palindrome){
+
+    Palindrome = '';
     
+
+
+
+
+ } 
+
+
